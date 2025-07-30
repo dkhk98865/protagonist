c256616464ed74cfcc5739f1fb591e8e5b8b66c8
Daniel Kim <dkhk98865@gmail.com>
2025-07-30 08:01:19 -0400

    navbar initial setup

--- components/navitems.tsx
+++ components/navitems.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const navItems = [
     { label:'Home', href: '/' },
@@ -8,10 +12,15 @@ const navItems = [
 ]
 
 const NavItems = () => {
+    const pathname = usePathname();
+
     return (
         <nav className="flex items-center gap-4">
             {navItems.map(({ label, href }) => (
-                <Link href={href} key={label}>
+                <Link 
+                    href={href} 
+                    key={label} 
+                    className={cn(pathname === href && 'text-primary font-semibold')}>
                     {label}
                 </Link>
             ))}
